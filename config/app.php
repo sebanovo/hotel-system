2cbb205fd4e90757fae28a0554ed90dab908d15a
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2025-05-25 21:32:36 -0400

    change zona horaria a LaPaz

--- config/app.php
+++ config/app.php
@@ -63,13 +63,13 @@ return [
     | Application Timezone
     |--------------------------------------------------------------------------
     |
-    | Here you may specify the default timezone for your application, which
+    | Here you may specify the default  timezonefor your application, which
     | will be used by the PHP date and date-time functions. We have gone
     | ahead and set this to a sensible default for you out of the box.
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/La_Paz', // default = 'UTC'
 
     /*
     |--------------------------------------------------------------------------
